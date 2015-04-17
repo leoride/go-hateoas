c75032af2617357eee667c43bcce272339ade77f
Tom <tomgranger59685@gmail.com>
2015-04-17 04:02:40 -0400

    Review test to add resource and functions for resourceHandler (for type assertion test)

--- hateoas_test.go
+++ hateoas_test.go
@@ -5,23 +5,43 @@ import (
 	"testing"
 )
 
+type testResource struct {
+	Id string
+}
+
+func (r testResource) MarshalJSON() ([]byte, error) {
+	return nil, nil
+}
+func (r testResource) UnmarshalJSON(b []byte) error {
+	return nil
+}
+
 type testResourceHandler struct {
 }
 
 func (rh testResourceHandler) ResourceName() string {
 	return "resources"
 }
-func (rh testResourceHandler) GetOne(string) (Resource, *Error) {
-	return nil, nil
+func (rh testResourceHandler) GetOne(id string) (Resource, *Error) {
+	testResource := testResource{}
+	return testResource, nil
 }
-func (rh testResourceHandler) GetAll(PageOpts) ([]Resource, *Error) {
-	return nil, nil
+func (rh testResourceHandler) GetAll(pageOpts PageOpts) ([]Resource, *Error) {
+	testResources := []testResource{}
+
+	resources := make([]Resource, len(testResources))
+	for index, value := range testResources {
+		resources[index] = value
+	}
+
+	return resources, nil
 }
-func (rh testResourceHandler) Create(Resource) (string, *Error) {
+func (rh testResourceHandler) Create(newR Resource) (string, *Error) {
 	return "", nil
 }
-func (rh testResourceHandler) Update(string, Resource) (Resource, *Error) {
-	return nil, nil
+func (rh testResourceHandler) Update(id string, updR Resource) (Resource, *Error) {
+	testResource := updR.(testResource)
+	return testResource, nil
 }
 func (rh testResourceHandler) Delete(string) *Error {
 	return nil
